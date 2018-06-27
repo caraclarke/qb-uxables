fec9573d6716f82f2c0dee8f310257db3fc44385
caraclarke <cara.clarke8@gmail.com>
2018-06-27 10:56:56 -0400

    rough modal and rail swap

--- app/js/build/main.js
+++ app/js/build/main.js
@@ -1,3 +1,36 @@
 "use strict";
 
-console.log("hallo");
+var rail = document.querySelectorAll(".rail");
+var greenRail = document.querySelector(".green-rail");
+var orangeRail = document.querySelector(".orange-rail");
+var greenTrigger = document.querySelector(".green-trigger");
+var orangeTrigger = document.querySelector(".orange-trigger");
+
+rail.forEach(function (item) {
+  item.addEventListener("click", function (e) {
+    $("#color-modal").modal("toggle");
+  });
+});
+
+// sidebar swaps
+orangeTrigger.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (orangeRail.classList.contains("hide")) {
+    orangeRail.classList.remove("hide");
+    greenRail.classList.add("hide");
+  } else {
+    orangeRail.classList.remove("hide");
+    greenRail.classList.add("hide");
+  }
+});
+
+greenTrigger.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (greenRail.classList.contains("hide")) {
+    greenRail.classList.remove("hide");
+    orangeRail.classList.add("hide");
+  } else {
+    orangeRail.classList.remove("hide");
+    greenRail.classList.add("hide");
+  }
+});
--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,34 @@
-console.log("hallo")
+const rail = document.querySelectorAll(".rail");
+const greenRail = document.querySelector(".green-rail");
+const orangeRail = document.querySelector(".orange-rail");
+const greenTrigger = document.querySelector(".green-trigger");
+const orangeTrigger = document.querySelector(".orange-trigger");
+
+rail.forEach((item) => {
+  item.addEventListener("click", ( e ) => {
+    $("#color-modal").modal("toggle");
+  });
+});
+
+// sidebar swaps
+orangeTrigger.addEventListener("click", ( e ) => {
+  e.preventDefault();
+  if ( orangeRail.classList.contains("hide")) {
+    orangeRail.classList.remove("hide");
+    greenRail.classList.add("hide");
+  } else {
+    orangeRail.classList.remove("hide");
+    greenRail.classList.add("hide");
+  }
+});
+
+greenTrigger.addEventListener("click", ( e ) => {
+  e.preventDefault();
+  if ( greenRail.classList.contains("hide")) {
+    greenRail.classList.remove("hide");
+    orangeRail.classList.add("hide");
+  } else {
+    orangeRail.classList.remove("hide");
+    greenRail.classList.add("hide");
+  }
+});
